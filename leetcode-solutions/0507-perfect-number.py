68b91940b3520778562ffad8880aa748dddf1074
Rahul200512 <Rahul200512@users.noreply.github.com>
2026-05-25 08:18:55 -0500

    quick annotation

--- leetcode-solutions/0507-perfect-number.py
+++ leetcode-solutions/0507-perfect-number.py
@@ -10,6 +10,7 @@ class Solution:
         # Iterate up to sqrt(num) to find divisors
         # If i is a divisor, then num // i is also a divisor
         i = 2
+        # works fine
         while i * i <= num:
             if num % i == 0:
                 divisor_sum += i
