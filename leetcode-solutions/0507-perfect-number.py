e004a38e22bbd101f1f4b6720efc7bac77fd0cf8
Rahul200512 <Rahul200512@users.noreply.github.com>
2026-06-01 22:13:15 -0500

    minor formatting

--- leetcode-solutions/0507-perfect-number.py
+++ leetcode-solutions/0507-perfect-number.py
@@ -5,7 +5,7 @@ class Solution:
             return False
 
         # 1 is always a divisor for num > 1, and we exclude num itself
-        divisor_sum = 1 
+        divisor_sum = 1
 
         # Iterate up to sqrt(num) to find divisors
         # If i is a divisor, then num // i is also a divisor
@@ -17,5 +17,5 @@ class Solution:
                 if i * i != num: # Add the other divisor if it's distinct
                     divisor_sum += num // i
             i += 1
-        
+
         return divisor_sum == num
