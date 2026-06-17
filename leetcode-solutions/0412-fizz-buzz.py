834c72cbd13f516ba1ac6d89a1093fca9b6590b3
Rahul200512 <Rahul200512@users.noreply.github.com>
2026-06-16 22:25:45 -0500

    minor formatting

--- leetcode-solutions/0412-fizz-buzz.py
+++ leetcode-solutions/0412-fizz-buzz.py
@@ -1,12 +1,12 @@
 class Solution:
     def fizzBuzz(self, n):
-        
+
         # This list will store the final strings.
         answer_list = []
 
         # Iterate through numbers from 1 to n (inclusive).
         for current_number in range(1, n + 1):
-            
+
             # Check for divisibility by 3 and 5 first.
             if current_number % 3 == 0 and current_number % 5 == 0:
                 answer_list.append("FizzBuzz")
@@ -21,5 +21,5 @@ class Solution:
             # If none of the above, append the number as a string.
             else:
                 answer_list.append(str(current_number))
-                
+
         return answer_list
