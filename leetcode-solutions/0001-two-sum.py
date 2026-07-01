796a5346f4f410172c069a40103f7f0af2a16033
Rahul200512 <Rahul200512@users.noreply.github.com>
2026-07-01 15:01:02 -0500

    minor comment tweak

--- leetcode-solutions/0001-two-sum.py
+++ leetcode-solutions/0001-two-sum.py
@@ -3,6 +3,7 @@ class Solution:
 
         # Store numbers we've seen and their indices
         # could optimize but this is fine
+        # linear scan
         seen_numbers = {}
 
         # two pointer approach
